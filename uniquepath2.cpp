df776f977f7a46a51c54df0a3fab10fd3d3482a2
rex <rohitrex321@gmail.com>
2024-02-08 00:05:12 +0530

    uniquepath2

--- uniquepath2.cpp
+++ uniquepath2.cpp
@@ -31,37 +31,46 @@ using namespace std;
 //     int ans  = help(m-1,n-1,obstacleGrid);
 //     return ans;
 // }
+
+
 // MEMOIZATION
-int help(int i,int j,vector<vector<int>> &obstacleGrid)
-{
-    if(i==0 && j==0)
-    {
-        if(obstacleGrid[i][j]==0)
-            return 1;
-        return 0;
-    }
-    if(i==0 && j!=0)
-    {
-        if(obstacleGrid[i][j]==0)
-            return help(i,j-1,obstacleGrid);
-        return 0;
-    }
-    if(i!=0 && j==0)
-    {
-        if(obstacleGrid[i][j]==0)
-            return help(i-1,j,obstacleGrid);
-        return 0;
-    }
-    if(obstacleGrid[i][j]==0)
-        return help(i-1,j,obstacleGrid)+help(i,j-1,obstacleGrid);
-    return 0;
-}
+// int help(int i,int j,vector<vector<int>> &obstacleGrid,vector<vector<int>>& dp)
+// {
+//     if(i==0 && j==0)
+//     {
+//         if(obstacleGrid[i][j]==0)
+//             return dp[i][j] = 1;
+//         return dp[i][j]=0;
+//     }
+//     if(i==0)
+//     {
+//         if(obstacleGrid[i][j]==0)
+//             if(dp[i][j]==-1)
+//                return dp[i][j] = help(i,j-1,obstacleGrid,dp);
+//             else
+//                 return dp[i][j];
+//         return 0;
+//     }
+//     if(j==0)
+//     {
+//         if(obstacleGrid[i][j]==0)
+//             if(dp[i][j]==-1)
+//                 return dp[i][j] = help(i-1,j,obstacleGrid,dp);
+//             else
+//                 return dp[i][j];
+//         return 0;
+//     }
+//     if(obstacleGrid[i][j]==0)
+//         if(dp[i][j]==-1)
+//             return dp[i][j] = help(i-1,j,obstacleGrid,dp)+help(i,j-1,obstacleGrid,dp);
+//         else
+//             return dp[i][j];
+//     return 0;
+// }
 int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
     int m = obstacleGrid.size();
     int n = obstacleGrid[0].size();
     vector <vector<int>> dp(m,vector<int>(n,-1));
-    int ans  = help(m-1,n-1,obstacleGrid);
-    return ans;
 }
 int main()
 {
